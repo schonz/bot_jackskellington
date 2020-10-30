10c19994e76534184cd4f09be50b654bc51e6cf8
Ubuntu <ubuntu@ubuntu.attlocal.net>
2020-10-30 03:52:32 +0000

    Init Commit

---
+++ src/jackskellington_controller/jackskellington_controller/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from jackskellington_controller.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/jackskellington_controller/jackskellington_controller/servoController.py
@@ -0,0 +1,10 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber)
