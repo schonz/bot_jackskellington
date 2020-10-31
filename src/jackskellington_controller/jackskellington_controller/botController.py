abbb0ec9dc38e87606acd28b9b4d9aeb11a5036e
Schonz <5674067+schonz@users.noreply.github.com>
2020-10-30 22:10:53 -0500

    init BotController node

---
+++ src/jackskellington_controller/jackskellington_controller/botController.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class BotController(Node):
+    def __init__(self):
+        super().__init__('BotController')
+        self.publisher = self.create_publisher(
+            String,
+            'servo_commands',
+            10
+        )
+
+        self.ch0Pos = 0
+        self.ch1Pos = 0
+        timer_period = 0.5  # sec
+        # -- temp --
+        self.ch0Dir = 1
+        self.ch1Dir = 1
+
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+
+    def timer_callback(self):
+        if self.ch0Pos >= 100:
+            self.ch0Dir = -1
+        elif self.ch0pos <= 0:
+            self.ch0Dir = 1
+
+        self.ch0Pos += self.ch0Dir
+
+        msg = String()
+        msg.data = '0:%d' % self.ch0Pos
